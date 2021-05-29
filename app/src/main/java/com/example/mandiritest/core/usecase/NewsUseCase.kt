102c9a91f2cf6f7aaf175be175402b7734898e01
FerdianSln <ferdiansln@gmail.com>
2021-05-29 21:54:52 +0700

    Update NewsUseCase.kt
    a little more description

--- app/src/main/java/com/example/mandiritest/core/usecase/NewsUseCase.kt
+++ app/src/main/java/com/example/mandiritest/core/usecase/NewsUseCase.kt
@@ -7,12 +7,15 @@ import com.example.mandiritest.model.SourceResponse
 import javax.inject.Inject
 
 class NewsUseCase @Inject constructor(private val repository: NewsRemoteRepository) {
+    // currently the country is limited, not all supported countries are here. But since this is just demo, current build should give you good general idea
     suspend fun getSources(
         country: String?, category: String?, language: String?
     ): ApiResponse<SourceResponse> {
         return repository.getSources(country, category, language)
     }
 
+    // currently not all are there, like domains, excludeDomains, because the ui will get bigger. but it is ready if needed. the general idea of domains & excludeDomains
+    // are represented by sources, they're pretty much the same
     suspend fun getArticles(
         q: String? = null,
         qInTitle: String? = null,
